689a958e0432f48085434d61bec7e4a89c09bdec
Omiadze <teoomiadze18@gmail.com>
2024-05-16 13:41:27 +0400

    change body background color for ios users

--- src/Components/Card/CreateCard.jsx
+++ src/Components/Card/CreateCard.jsx
@@ -4,10 +4,10 @@ const CreateCard = ({ card, onClick }) => {
       className="h-40 sm:h-48 md:h-80 md:w-64 w-20 sm:w-48 mb-4 transform transition duration-300 hover:scale-110 cursor-pointer"
       onClick={onClick}
     >
-      <div className="relative w-full">
+      <div className="relative w-full ">
         <img
           src={card?.photoUrl}
-          className="mb-3 h-20 sm:h-32 md:h-64 md:w-64 w-20 sm:w-64 rounded-xl"
+          className="mb-3 h-20 sm:h-32 md:h-64 md:w-64 w-20 sm:w-64 rounded-xl shadow-xl"
           alt=""
         />
       </div>
